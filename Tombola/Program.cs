ed3f1f262a9e91185cd50ae25da91a465364e1c5
Thomas Rota <thommyrota@x6n6j.onmicrosoft.com>
2023-01-06 17:08:04 +0100

    Stampa Tabellone

--- Tombola/Program.cs
+++ Tombola/Program.cs
@@ -10,7 +10,31 @@ namespace Tombola
     {
         static void Main(string[] args)
         {
+            //Dichiarazione matrice
+            int[,] tabellone = new int[9, 10];
+            int numero = 1;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tabellone[i, j] = numero;
+                    numero++;
+                }
+            }
 
+            // Stampa del tabellone
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int x = j + 1;  // Posizione della colonna
+                    int y = i + 1;  // Posizione della riga
+                    int left = x * 4 - 3;  // Spazio tra le colonne
+                    int top = y * 2 - 1;  // Spazio tra le righe
+                    Console.SetCursorPosition(left, top);
+                    Console.Write(tabellone[i, j]);
+                }
+            }
         }
     }
 }
