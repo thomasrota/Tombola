ebc986df27bed70f77531215579a5f7f8f4ee7fe
Thomas Rota <thommyrota@x6n6j.onmicrosoft.com>
2023-01-08 16:05:56 +0100

    Stampa Cartelle

--- Tombola/Program.cs
+++ Tombola/Program.cs
@@ -40,10 +40,14 @@ namespace Tombola
                     Console.Write(tabellone[i, j]);
                 }
             }
+
             // Chiama la funzione per estrarre e visualizzare il numero
-            EstraiNumero(nestratti);
+            
+
+            // Chiama la funzione per generare e stampare le cartelle
+            GeneraCartelle();
         }
-        static void EstraiNumero(List<int> nestratti)
+        static void EstraiNumero(List<int> nestratti, int[,] tabellone)
         {
             // Genera un numero casuale finché non viene estratto un numero che non è stato ancora estratto
             Random random = new Random();
@@ -61,6 +65,62 @@ namespace Tombola
             Console.WriteLine("Numero estratto:");
             Console.SetCursorPosition(45, 1);
             Console.WriteLine(numeroEstratto);
+
+            // Cambia il colore del numero estratto nel tabellone
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (tabellone[i, j] == numeroEstratto)
+                    {
+                        int x = j + 1;  // Posizione della colonna
+                        int y = i + 1;  // Posizione della riga
+                        int left = x * 4 - 3;  // Spazio tra le colonne
+                        int top = y * 2 - 1;  // Spazio tra le righe
+                        Console.SetCursorPosition(left, top);
+                        Console.Write(tabellone[i, j]);
+                    }
+                }
+            }
+        }
+        static void GeneraCartelle()
+        {
+            Random random = new Random();
+            int[,] cartella = new int[3, 5];
+            int[,] cartella2 = new int[3, 5];
+            int righe = 3, colonne = 5;
+            int x = 2, y = 22, x2 = 22, y2 = 22;
+
+            // stampa cartella 1
+            Console.SetCursorPosition(2, 20);
+            Console.WriteLine("Cartella 1");
+            for (int i = 0; i < righe; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                for (int j = 0; j < colonne; j++)
+                {
+                    cartella[i, j] = random.Next(1, 91);
+                    Console.Write(cartella[i, j] + " ");
+                }
+                Console.WriteLine();
+                y++;
+            }
+
+            // stampa cartella 2
+            Console.SetCursorPosition(22, 20);
+            Console.WriteLine("Cartella 2");
+            for (int i = 0; i < righe; i++)
+            {
+                Console.SetCursorPosition(x2, y2);
+                for (int j = 0; j < colonne; j++)
+                {
+                    cartella2[i, j] = random.Next(1, 91);
+                    Console.Write(cartella2[i, j] + " ");
+                }
+                Console.WriteLine();
+                y2++;
+            }
         }
     }
 }
